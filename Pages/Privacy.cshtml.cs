3603922d604f7635648c93c1dcaf16055b59ecfe
laksh rao <121564518+thelakshrao@users.noreply.github.com>
2026-03-02 21:54:12 +0530

    Challenge: Server Time App complete

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServerTimeApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
